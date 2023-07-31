68089219634a629e468fcdac6f590cc2f1e28ad0
송민석 <songms0909@khu.ac.kr>
2023-07-31 17:47:45 +0900

    Feat : API 연동 후 초기 일기 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useState, useRef } from "react";
-import Lifecycle from "./Lifecycle";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
   //일기 배열(data)에 새로운 일기를 추가할 수 있는 함수
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments").then((res) =>
+      res.json()
+    );
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -34,7 +55,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList
         onEdit={onEdit}
